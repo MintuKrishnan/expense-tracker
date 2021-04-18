3442f01aae102e078a3e926ea18b37eac49f36b3
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-04-18 09:40:09 +0530

    added files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
-  const expenseDate = new Date().toLocaleString();
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
-
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">
+          <span styleName="font-family:Arial;">&#8377; </span>
+          {props.amount}
+        </div>
       </div>
     </div>
   );
